78bb6c52b46e6f19c3aea95117e2ef6c2584dff3
vuillaume <vitalvuillaume@proton.me>
2025-04-16 18:53:17 +0200

    fix bug

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -8,13 +9,24 @@ from selenium.webdriver.common.keys import Keys
 
 
 
-username = input("Ecrivez votre nom d'utilisateur : ")
+try:
+    with open("data.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+        username = data["username"]
+        cours = data["cours"]
+        graphic = data["graphic"]
+
+        print(f"Si vous voulez changer : {username}, {cours}, {graphic}, allez dans data.json")
+except:
+    username = input("Ecrivez votre nom d'utilisateur : ")
+    cours = input("Ecrivez la date de création du JT, exemple : 15.04.2025 : ")
+    graphic = input("Si vous voulez avoir l'interface, ecrivez yes sinon mettez autres choses : ")
+
 password = input("Ecrivez votre mot de passe : ")
-cours = input("Ecrivez la date de création du JT, exemple : 15.04.2025 : ")
 date = input("Ecrivez le jour du cours, exemple : 16.04.2025 : ")
 periode = input("Ecrivez nombre de periode : ")
 inputDo = input("Ecrivez en bref le ce que vous avez fait pendant le cours, exemple : html et css mais les bases : ")
-graphic = input("Si vous voulez avoir l'interface ecrivez yes sinon mettez autres choses : ")
 
 
 
@@ -39,9 +51,9 @@ chrome_service = Service(executable_path='/usr/bin/chromedriver')
 
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
+
+
 driver.get("https://jt.s2.rpn.ch")
-
-
 
 time.sleep(2)
 
@@ -93,7 +105,21 @@ btnSave.click()
 
 
 
-if graphic.lower() != "yes":
+data = {
+    "username": username,
+    "cours": cours,
+    "graphic": graphic
+}
+
+with open("data.json", "w", encoding="utf-8") as file:
+    json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+
+retry = input("Si vous voulez recommencer, ecrivez yes sinon mettez autres choses : ")
+
+if retry.lower() != "yes":
     driver.quit()
 else:
-    input()
+    driver.quit()
+    subprocess.run(["python3", __file__])
